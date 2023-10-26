c94cbc06667d8c4515f1ea11b6ed5f036d387e0a
Elena Datsenko <elenasheglova15414@gmail.com>
2023-10-25 19:38:11 -0700

    task2

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -4,13 +4,13 @@ function solveEquation(a, b, c) {
   const arr = [];
   const d = (b * b) - (4 * a * c);
   if (d > 0) {
-    const x1 = (-b + Math.sqrt(d) ) / (2*a);
-    const x2 = (-b - Math.sqrt(d) ) / (2*a);
+    const x1 = (-b + Math.sqrt(d)) / (2 * a);
+    const x2 = (-b - Math.sqrt(d)) / (2 * a);
     arr.push(x1);
     arr.push(x2);
 
   } else if (d === 0) {
-    const x = (- b / ( 2 * a ));
+    const x = (- b / (2 * a));
     arr.push(x);
   }
 
@@ -18,5 +18,23 @@ function solveEquation(a, b, c) {
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+
+  if (
+    typeof percent != "number" ||
+    typeof contribution != "number" ||
+    typeof amount != "number" ||
+    typeof countMonths != "number"
+  ) {
+    return "Ошибка ввода данных";
+  }
+  if (percent < 0 || percent > 100) {
+    return "Ошибка ввода данных";
+  }
+
+  const S = amount - contribution;
+  const P = percent / 100 / 12;
+
+  const monthlyPayment = S * (P + (P / (((1 + P) ** countMonths) - 1)));
+  const totalAmount = parseFloat((monthlyPayment * countMonths).toFixed(2));
+  return totalAmount;
 }

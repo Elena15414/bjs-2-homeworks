b45f0c80b01bbb541b3567a8a9a6ba1bd749dd1c
Elena Datsenko <elenasheglova15414@gmail.com>
2023-11-20 00:24:41 -0800

    done

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,50 @@
-﻿
+﻿function parseCount(value) {
+    const result = parseFloat(value);
+    if (isNaN(result)) {
+        throw new Error("Невалидное значение");
+    }
+    return result;
+}
+
+function validateCount(value2) {
+    try {
+        const result2 = parseCount(value2);
+        return result2;
+    } catch (error) {
+        return error;
+    }
+}
+
+class Triangle {
+    constructor(a, b, c) {
+        if (a + b < c || b + c < a || c + a < b) {
+            throw new Error("Треугольник с такими сторонами не существует")
+        }
+        this.a = a;
+        this.b = b;
+        this.c = c;
+
+    }
+    get perimeter() {
+        return this.a + this.b + this.c;
+    }
+    get area() {
+        const p = 0.5 * this.perimeter;
+        return +Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c)).toFixed(3);
+    }
+}
+function getTriangle(a, b, c) {
+    try {
+        return new Triangle(a, b, c);
+    } catch (error) {
+        return {
+            get perimeter() {
+                return "Ошибка! Треугольник не существует";
+            },
+            get area() {
+                return "Ошибка! Треугольник не существует";
+            }
+
+        }
+    }
+}

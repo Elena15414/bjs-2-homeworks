7d8951a4c4fd0442c0178bb590fa3409eaa68379
Elena Datsenko <elenasheglova15414@gmail.com>
2023-12-11 00:22:57 -0800

    done

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,9 +1,49 @@
 //Задача № 1
 function cachingDecoratorNew(func) {
-  
+    let cache = [];
+
+    function wrapper(...args) {
+
+        const hash = md5(args);
+
+        const objectInCache = cache.find((item) => item.hash === hash);
+        if (objectInCache) {
+            console.log("Из кеша: " + objectInCache.value);
+            return "Из кеша: " + objectInCache.value;
+        } else {
+            const value = func(...args);
+            cache.push({ hash, value });
+            if (cache.length > 5) {
+                cache.shift();
+            }
+            console.log("Вычисляем: " + value);
+            return "Вычисляем: " + value;
+        }
+    }
+    return wrapper;
 }
 
 //Задача № 2
 function debounceDecoratorNew(func, delay) {
-  
+    let timeoutId;
+
+    function wrapper(...args) {
+        wrapper.allCount++;
+
+        if (timeoutId) {
+            clearTimeout(timeoutId);
+        } else {
+            func(args);
+            wrapper.count++;
+        }
+        timeoutId = setTimeout(() => {
+            func(args);
+            wrapper.count++;
+        }, delay);
+
+    }
+    wrapper.allCount = 0;
+    wrapper.count = 0;
+
+    return wrapper;
 }
